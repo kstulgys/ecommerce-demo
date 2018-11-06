0a537617a592612dd7f926c6520ad9a33c4975f8
kstulgys <karolis.stulgys@gmail.com>
2018-11-06 14:49:32 +1100

    update heroku yoga variables

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 const { server } = require('./server')
-const { PRISMA_ENDPOINT, APP_URL } = require('./config')
+// const { PRISMA_ENDPOINT, APP_URL } = require('./config')
 
+//start it!
 server.start(() => console.log(`Server is running on http://localhost:4000`))
