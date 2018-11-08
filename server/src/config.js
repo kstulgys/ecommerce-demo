79d1a8dba33ac378059efcc1310f4f5d7909f929
kstulgys <karolis.stulgys@gmail.com>
2018-11-09 10:37:04 +1100

    work in progress

--- server/src/config.js
+++ server/src/config.js
@@ -1,16 +1,4 @@
 const APP_URL = `https://ecomm-client-prod.herokuapp.com`
-// const PRISMA_ENDPOINT = `https://ecommerce-prod-19bfeff3b1.herokuapp.com/ecomm-prisma-prod/prod`
 
 module.exports = { APP_URL }
 
-// export const {
-//   PRISMA_ENDPOINT = 'https://us1.prisma.sh/karolis-stulgys-9eb3b5/ecommerce-demo-database/dev',
-//   PRISMA_SECRET = 'mysecret123',
-//   APP_SECRET = 'jwtsecret123',
-//   APP_URL = 'http://localhost:3000',
-//   SENDGRID_API_KEY = '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',
-//   STRIPE_SECRET = 'sk_test_wPZSBA8J4N9WYxybF4zh8ei7',
-//   NODE_ENV = 'development',
-// } = process.env
-
-// export const IN_PROD = NODE_ENV === 'production'
