e1f3e5f72fe864e42cb150457fce7e76481b6ba4
Karolis S <karolis.stulgys@gmail.com>
2018-11-09 13:06:54 +1100

    Updates

---
+++ precache-manifest.b3b574a992b667d068507bb2f8a03def.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d254f6b052a6ebcd5547a94860d99e63",
+    "url": "/my-app/static/media/tuple.d254f6b0.mjs"
+  },
+  {
+    "revision": "497f5ee233e7f7298788451aef6645d7",
+    "url": "/my-app/static/media/index.497f5ee2.mjs"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "3b6caab542907fc46c2a",
+    "url": "/my-app/static/js/main.3b6caab5.chunk.js"
+  },
+  {
+    "revision": "d2da34fe75576b86c957",
+    "url": "/my-app/static/js/1.d2da34fe.chunk.js"
+  },
+  {
+    "revision": "3b6caab542907fc46c2a",
+    "url": "/my-app/static/css/main.7e696ae8.chunk.css"
+  },
+  {
+    "revision": "d2da34fe75576b86c957",
+    "url": "/my-app/static/css/1.d70ac2f7.chunk.css"
+  },
+  {
+    "revision": "fba8f0d01e17e2ae8c90dc6583bb04e1",
+    "url": "/my-app/index.html"
+  }
+];
