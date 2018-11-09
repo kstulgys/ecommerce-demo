158b550563096a329fa0b2b283544d9a78fac895
kstulgys <karolis.stulgys@gmail.com>
2018-11-09 15:22:07 +1100

    work in progress

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
 export const endpoint = `http://localhost:4000`
-export const prodEndpoint = `https://ecomm-yoga-prod.herokuapp.com/`
+export const prodEndpoint = `https://ecomm-yoga-prod.herokuapp.com`
