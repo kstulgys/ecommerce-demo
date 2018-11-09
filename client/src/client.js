2698fb39a8293b86d8440a2cce24f165f98a7e9d
kstulgys <karolis.stulgys@gmail.com>
2018-11-09 11:04:13 +1100

    cleaned client.js

--- client/src/client.js
+++ client/src/client.js
@@ -7,64 +7,70 @@ import { withClientState } from 'apollo-link-state'
 import { resolvers, defaults } from './resolvers'
 import { endpoint, prodEndpoint } from './config'
 
-// uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
+const tokenValue = localStorage.getItem(AUTH_TOKEN)
 
 const httpLink = new HttpLink({
   uri:
     process.env.NODE_ENV === 'development'
       ? `http://${endpoint}`
       : `http://${prodEndpoint}`,
-})
-
-const middlewareLink = new ApolloLink((operation, forward) => {
-  // get the authentication token from local storage if it exists
-  const tokenValue = localStorage.getItem(AUTH_TOKEN)
-  // console.log('the token', tokenValue)
-  // return the headers to the context so httpLink can read them
-  operation.setContext({
-    headers: {
-      Authorization: tokenValue ? `Bearer ${tokenValue}` : '',
-    },
-  })
-  return forward(operation)
-})
-
-// authenticated httplink
-const httpLinkAuth = middlewareLink.concat(httpLink)
-
-const wsLink = new WebSocketLink({
-  uri:
-    process.env.NODE_ENV === 'development'
-      ? `ws://${endpoint}`
-      : `ws://${prodEndpoint}`,
-  options: {
-    reconnect: true,
-    // connectionParams: {
-    //   Authorization: tokenValue ? `Bearer ${tokenValue}` : '',
-    // },
+  headers: {
+    Authorization: tokenValue ? `Bearer ${tokenValue}` : '',
   },
 })
 
-const link = split(
-  // split based on operation type
-  ({ query }) => {
-    const { kind, operation } = getMainDefinition(query)
-    return kind === 'OperationDefinition' && operation === 'subscription'
-  },
-  wsLink,
-  httpLinkAuth,
-)
-
-// apollo client setup
 const cache = new InMemoryCache()
 const stateLink = withClientState({ cache, defaults, resolvers })
+const link = ApolloLink.from([stateLink, httpLink])
+
 const client = new ApolloClient({
-  link: ApolloLink.from([stateLink, link]),
+  link,
   cache,
-  connectToDevTools: true,
+  // connectToDevTools: true,
 })
 
 export default client
+
+// const middlewareLink = new ApolloLink((operation, forward) => {
+//   // get the authentication token from local storage if it exists
+//   const tokenValue = localStorage.getItem(AUTH_TOKEN)
+//   // console.log('the token', tokenValue)
+//   // return the headers to the context so httpLink can read them
+//   operation.setContext({
+//     headers: {
+//       Authorization: tokenValue ? `Bearer ${tokenValue}` : '',
+//     },
+//   })
+//   return forward(operation)
+// })
+
+// authenticated httplink
+// const httpLinkAuth = middlewareLink.concat(httpLink)
+
+// const wsLink = new WebSocketLink({
+//   uri:
+//     process.env.NODE_ENV === 'development'
+//       ? `ws://${endpoint}`
+//       : `ws://${prodEndpoint}`,
+//   options: {
+//     reconnect: true,
+//     // connectionParams: {
+//     //   Authorization: tokenValue ? `Bearer ${tokenValue}` : '',
+//     // },
+//   },
+// })
+
+// const link = split(
+//   // split based on operation type
+//   ({ query }) => {
+//     const { kind, operation } = getMainDefinition(query)
+//     return kind === 'OperationDefinition' && operation === 'subscription'
+//   },
+//   // wsLink,
+//   httpLinkAuth,
+// )
+// apollo client setup
+// const link = ApolloLink.from([httpLink]);
 
 // const resolvers = {
 // 	Mutation: {
